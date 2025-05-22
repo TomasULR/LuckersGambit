2b1807d197ff0498b32e911322b9fc6b38713895
tomas.ulrych <tomas.ulrych1@gmail.com>
2025-05-22 10:31:26 +0200

    Implementace PlayerStats.java

--- src/assets/PlayerStats.java
+++ src/assets/PlayerStats.java
@@ -4,21 +4,25 @@ import model.GameResult;
 import model.Player;
 
 import java.util.*;
-import java.util.stream.Collectors;
-
 public class PlayerStats {
 
     public static void printStats(Player player) {
-        List<GameResult> history = player.getHistory();
 
-        System.out.println("=== Player Statistics ===");
-        System.out.println("Hráč: " + player.getNickName());
-        System.out.println("Současný stav: " + player.getBalance());
-        System.out.println("Total Games Played: " + history.size());
-        System.out.println("Počet vyhraných her: " + countWins(history));
-        System.out.println("Prohry: " + countLosses(history));
-        System.out.println("Zisk/ztráta" + totalWinnings(history));
-        System.out.println("Win rate" + getWinRate(history));
+        List<GameResult> history = player.getHistory();
+        if (!(history.isEmpty())){
+            System.out.println("=== Player Statistics ===");
+            System.out.println("Hráč: " + player.getNickName());
+            System.out.println("Současný stav: " + player.getBalance());
+            System.out.println("Total Games Played: " + history.size());
+            System.out.println("Počet vyhraných her: " + countWins(history));
+            System.out.println("Prohry: " + countLosses(history));
+            System.out.println("Zisk/ztráta " + totalWinnings(history));
+            System.out.println("Win rate " + getWinRate(history));
+        }
+        else {
+            System.out.println("Historie je zatím prázdná...");
+        }
+
     }
 
     public static long countWins(List<GameResult> history) {
@@ -26,14 +30,28 @@ public class PlayerStats {
     }
 
     public static long countLosses(List<GameResult> history) {
-        return 0;
+        int lossConut = 0;
+        for (GameResult g: history) {
+            if (!(g.isWin())){
+                lossConut++;
+            }
+        }
+        return lossConut;
     }
 
     public static int totalWinnings(List<GameResult> history) {
-        return 0;
+        int winSum = 0;
+        for (GameResult g: history) {
+            if (g.isWin()){
+                winSum += g.getWinnings();
+            }
+        }
+        return winSum;
     }
 
     public static String getWinRate(List<GameResult> history) {
-        return "X %";
+        int gamesCount = history.size();
+        double x = ((double) countWins(history) /gamesCount) * 100;
+        return x + " %";
     }
 }
